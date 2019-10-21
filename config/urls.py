bb28337d0d2fd40e6dd08ec1b7f8f6e17c0c1ce5
Pavel Vergeev <pvergeev@pm.me>
2019-10-21 21:51:46 +0300

    Remove unnecessary empty views

--- config/urls.py
+++ config/urls.py
@@ -2,14 +2,11 @@ from django.conf import settings
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView
 from django.views import defaults as default_views
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path(
-        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
-    ),
+    path("", RedirectView.as_view(pattern_name="users:redirect", permanent=True), name="home"),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
