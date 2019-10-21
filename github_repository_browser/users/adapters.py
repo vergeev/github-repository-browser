d77aa7b88838be593716a94cbb3b7150791e2a2a
Pavel Vergeev <pvergeev@pm.me>
2019-10-21 22:55:18 +0300

    Leave Github the only login option

--- github_repository_browser/users/adapters.py
+++ github_repository_browser/users/adapters.py
@@ -8,7 +8,8 @@ from django.http import HttpRequest
 
 class AccountAdapter(DefaultAccountAdapter):
     def is_open_for_signup(self, request: HttpRequest):
-        return getattr(settings, "ACCOUNT_ALLOW_REGISTRATION", True)
+        # Disallow registration without Github
+        return False
 
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
