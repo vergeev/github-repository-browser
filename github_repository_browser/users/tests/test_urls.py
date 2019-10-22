935470095cdc28b21e1cc265c93b79fde00f5b09
Pavel Vergeev <pvergeev@pm.me>
2019-10-22 10:51:59 +0300

    Fix tests by removing irrelevant ones

--- github_repository_browser/users/tests/test_urls.py
+++ github_repository_browser/users/tests/test_urls.py
@@ -13,11 +13,6 @@ def test_detail(user: settings.AUTH_USER_MODEL):
     assert resolve(f"/users/{user.username}/").view_name == "users:detail"
 
 
-def test_update():
-    assert reverse("users:update") == "/users/~update/"
-    assert resolve("/users/~update/").view_name == "users:update"
-
-
 def test_redirect():
     assert reverse("users:redirect") == "/users/~redirect/"
     assert resolve("/users/~redirect/").view_name == "users:redirect"
